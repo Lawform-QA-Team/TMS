75a423e4d0cc5ca1e9221d11b7c6af689b9098e2
LimHeekun <lheekun0515@amicuslex.net>
2026-03-03 18:21:52 +0900

    feat: 필터링 관리 조회 단위 작성

---
+++ test-scripts/performance/samsung/real/web/filtering/filtering.js
@@ -0,0 +1,117 @@
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js"
+import { URLS } from '../../url_base_sam.js';
+import { SELECTORS } from '../../selector_sam.js';
+import { getFormattedTimestamp } from '../../../../common/utils.js';
+import { selectComboboxOption } from '../../../../common/combobox_helper.js';
+import { browser } from 'k6/browser';
+import { getCredentials, loginWithPage } from '../login/login_helper.js';
+
+export const options = {
+    scenarios: {
+        ui: {
+            executor: 'shared-iterations',
+            vus: 1,
+            iterations: 1,
+            options: {
+                browser: {
+                    type: 'chromium',
+                },
+            },
+        },
+    },
+    thresholds: {
+        checks: ['rate==1.0'],
+    },
+};
+
+async function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export default async function() {
+    const page = await browser.newPage();
+    const credentials = getCredentials();
+    const getNewTimeStamp = () => getFormattedTimestamp().replace(/\s/g, '_');
+
+    try {
+        await loginWithPage(page, credentials);
+
+        // 필터링 관리
+        await page.goto(URLS.FILTERING.FILTERING);
+        let timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING.png` });
+        await wait(5000);
+
+        // 필터링 관리 페이지네이션
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.PAGINATION);
+        await page.click(SELECTORS.COMMON.PAGE_LAST);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_pagination_last.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.PAGINATION);
+        await page.click(SELECTORS.COMMON.PAGE_FIRST);
+
+        // 필터링 관리 검색
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.INPUT_SEARCH);
+        await page.type(SELECTORS.ADMIN.FILTERING.INPUT_SEARCH, '필터');
+        await page.waitForSelector(SELECTORS.COMMON.SEARCH);
+        await page.click(SELECTORS.COMMON.SEARCH);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_search.png` });
+
+        // 필터링 관리 필터링 등록 진입
+        await page.goto(URLS.FILTERING.FILTERING);
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.BUTTON_REGISTER_CLICK);
+        await page.click(SELECTORS.ADMIN.FILTERING.BUTTON_REGISTER_CLICK);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_register.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.BUTTON_CLOSE);
+        await page.click(SELECTORS.ADMIN.FILTERING.BUTTON_CLOSE);
+
+        // 필터링 관리 필터링 등록
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.BUTTON_REGISTER_CLICK);
+        await page.click(SELECTORS.ADMIN.FILTERING.BUTTON_REGISTER_CLICK);
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.INPUT);
+        await page.type(SELECTORS.ADMIN.FILTERING.INPUT, '필터링 단어 테스트');
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.INPUT_1);
+        await page.type(SELECTORS.ADMIN.FILTERING.INPUT_1, '필터링 사유 테스트');
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_register_write.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.BUTTON_SUBMIT);
+        await page.click(SELECTORS.ADMIN.FILTERING.BUTTON_SUBMIT);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_register_submit.png` });
+
+        // 필터링 관리 테이블 클릭
+        await page.waitForSelector(SELECTORS.COMMON.TABLE);
+        await page.click(`${SELECTORS.COMMON.TABLE} button`);
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.INPUT);
+        await page.type(SELECTORS.ADMIN.FILTERING.INPUT, '필터링 단어 테스트 2');
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.INPUT_1);
+        await page.type(SELECTORS.ADMIN.FILTERING.INPUT_1, '필터링 사유 테스트 2');
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.SWITCH);
+        await page.click(SELECTORS.ADMIN.FILTERING.SWITCH);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_register_edit.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.FILTERING.BUTTON_SUBMIT);
+        await page.click(SELECTORS.ADMIN.FILTERING.BUTTON_SUBMIT);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_FILTERING_register_edit_submit.png` });
+
+    } finally {
+        await page.close();
+    }
+}
+
+export function handleSummary(data) {
+    const timestamp = getFormattedTimestamp().replace(/\s/g, '_');
+    return {
+        [`Result/notice_${timestamp}.html`]: htmlReport(data),
+    };
+}
