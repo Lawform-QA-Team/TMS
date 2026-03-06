c723e6792d25abe733da27f02f0e2b6f42dafda6
LimHeekun <lheekun0515@amicuslex.net>
2026-03-06 17:24:17 +0900

    feat: 개인정보처리방침 동작 추가

--- test-scripts/performance/samsung/real/admin/service_terms/service_terms.js
+++ test-scripts/performance/samsung/real/admin/service_terms/service_terms.js
@@ -46,14 +46,14 @@ export default async function() {
         let timestamp = getNewTimeStamp();
         await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY.png` });
 
-        // 약관 관리 - 개인정보처리방침, 페이지네이션 -> 데이터 부족
-        // await page.waitForSelector(SELECTORS.ADMIN.TERMS.PAGINATION);
-        // await page.click(SELECTORS.COMMON.PAGE_LAST);
-        // await wait(5000);
-        // timestamp = getNewTimeStamp();
-        // await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_pagination_last.png` });
-        // await page.waitForSelector(SELECTORS.ADMIN.TERMS.PAGINATION);
-        // await page.click(SELECTORS.COMMON.PAGE_FIRST);
+        // 약관 관리 - 개인정보처리방침, 페이지네이션
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.PAGINATION);
+        await page.click(SELECTORS.COMMON.PAGE_LAST);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_pagination_last.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.PAGINATION);
+        await page.click(SELECTORS.COMMON.PAGE_FIRST);
 
         // 약관 관리 - 개인정보처리방침, 검색
         await selectDateRangeInRdpCalendar(page, SELECTORS.ADMIN.TERMS.DATEPICKER, SELECTORS.ADMIN.TERMS.DATEPICKER_START, '2026-02-01', '2026-02-28')
@@ -62,14 +62,6 @@ export default async function() {
         await wait(5000);
         timestamp = getNewTimeStamp();
         await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_search.png` });
-        await page.goto(URLS.SERVICE.PRIVACY);
-
-        // 약관 관리 - 개인정보처리방침, 테이블 클릭 -> 연결 안됨
-        await page.waitForSelector(SELECTORS.COMMON.TABLE);
-        await page.click(SELECTORS.COMMON.TABLE);
-        await wait(5000);
-        timestamp = getNewTimeStamp();
-        await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_table.png` });
         await page.goto(URLS.SERVICE.PRIVACY);
         
         // 약관 관리 - 개인정보처리방침, 등록 진입
@@ -93,12 +85,40 @@ export default async function() {
         timestamp = getNewTimeStamp();
         await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_register_write.png` });
         
-        // 약관 관리 - 개인정보처리방침, 등록 -> 동작 안됨
+        // 약관 관리 - 개인정보처리방침, 등록
         await page.waitForSelector(SELECTORS.ADMIN.TERMS.BUTTON_SUBMIT);
         await page.click(SELECTORS.ADMIN.TERMS.BUTTON_SUBMIT);
         await wait(5000);
         timestamp = getNewTimeStamp();
         await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_register_submit.png` });
+
+        // 약관 관리 - 개인정보처리방침, 테이블 클릭
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.TABLE_LIST);
+        await page.click(SELECTORS.COMMON.TABLE);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_table.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.BUTTON_LIST);
+        await page.click(SELECTORS.ADMIN.TERMS.BUTTON_LIST);
+
+        // 약관 관리 - 개인정보처리방침, 상세 수정
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.TABLE_LIST);
+        await page.click(SELECTORS.COMMON.TABLE);
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.RADIO_VISIBILITY_N);
+        await page.click(SELECTORS.ADMIN.TERMS.RADIO_VISIBILITY_N);
+        await selectRandomDateFromRdpCalendar(page, SELECTORS.ADMIN.TERMS.DATEPICKER_REVISION_DATE)
+        await page.waitForSelector(`[contenteditable="true"]`);
+        await page.type(`[contenteditable="true"]`, '수정 테스트');
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_edit.png` });
+
+        // 약관 관리 - 개인정보처리방침, 상세 수정 저장
+        await page.waitForSelector(SELECTORS.ADMIN.TERMS.BUTTON_SUBMIT);
+        await page.click(SELECTORS.ADMIN.TERMS.BUTTON_SUBMIT);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_SERVICE_PRIVACY_edit_submit.png` });
 
     } finally {
         if (page) await page.close();
