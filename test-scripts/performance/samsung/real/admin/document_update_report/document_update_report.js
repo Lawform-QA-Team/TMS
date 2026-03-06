f2bfb79e77ec354164f3f4f3c3df072d3752317d
LimHeekun <lheekun0515@amicuslex.net>
2026-03-06 15:19:47 +0900

    feat: 문서 업데이트 리포트 최소 단위 코드 추가

--- test-scripts/performance/samsung/real/admin/document_update_report/document_update_report.js
+++ test-scripts/performance/samsung/real/admin/document_update_report/document_update_report.js
@@ -4,6 +4,7 @@ import { SELECTORS } from '../../selector_sam.js';
 import { getFormattedTimestamp } from '../../../../common/utils.js';
 import { browser } from 'k6/browser';
 import { getCredentials, loginWithPage } from '../login/login_helper.js';
+import { selectRandomDateFromRdpCalendar } from '../../../../common/datepicker_helper.js';
 import { sendSlackWebhook, buildK6SummaryMessage } from '../../../../common/slack_helper.js';
 
 export const options = {
@@ -39,11 +40,43 @@ export default async function() {
     try {
         await loginWithPage(page, credentials);
 
+        // 개발 중
         // 문서 업데이트 리포트
         await page.goto(URLS.DOCUMENT_UPDATE.LAW);
+        await wait(5000);
         let timestamp = getNewTimeStamp();
         await page.screenshot({ path: `screenshots/${timestamp}_DOCUMENT_UPDATE_LAW.png` });
+
+        // 
+        await selectRandomDateFromRdpCalendar(page, SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.DATEPICKER)
         await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_DOCUMENT_UPDATE_LAW_1.png` });
+
+        // 
+        await page.waitForSelector(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_HISTORY_CLICK);
+        await page.click(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_HISTORY_CLICK);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_DOCUMENT_UPDATE_LAW_2.png` });
+        await page.waitForSelector(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_CLOSE);
+        await page.click(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_CLOSE);
+
+        //
+        await page.waitForSelector(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_HISTORY_CLICK);
+        await page.click(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_HISTORY_CLICK);
+        await page.waitForSelector(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_CONFIRM);
+        await page.click(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_CONFIRM);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_DOCUMENT_UPDATE_LAW_3.png` });
+
+        //
+        await page.waitForSelector(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_VIEW_ORIGINAL);
+        await page.click(SELECTORS.ADMIN.DOCUMENT_UPDATE_REPORT.BUTTON_VIEW_ORIGINAL);
+        await wait(5000);
+        timestamp = getNewTimeStamp();
+        await page.screenshot({ path: `screenshots/${timestamp}_DOCUMENT_UPDATE_LAW_4.png` });
 
     } finally {
         if (page) await page.close();
