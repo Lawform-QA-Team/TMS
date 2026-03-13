4cd85b365b29f2ea3c78aeceb420ca091030be38
LimHeekun <lheekun0515@amicuslex.net>
2026-03-13 17:38:00 +0900

    fix: 미사용 파일 삭제

--- test-scripts/playwright/tests/samsung/real/web/autodoc/autodoc_ai.spec.js
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-import { TIMEOUT } from '../../../../../common/constants.js';
-import { run } from '../../../../../samsung/real/web/autodoc/autodoc_ai.js';
-
-test('samsung real web - autodoc ai', async ({ page }) => {
-  await run(page);
-}, TIMEOUT.TEST);
